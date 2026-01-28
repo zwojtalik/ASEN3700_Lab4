3ad1302aa1cc9cc62febb675b4cb31b1ca007629
zwojtalik <zawo6371@colorado.edu>
2026-01-28 16:40:51 -0700

    khb

--- main.m
+++ main.m
@@ -2,4 +2,14 @@ clc
 close all
 clear
 
-%%
+%% Getting variables
+load("lab04_analysis_signal1.mat");
+
+%% Problem 4.1 
+% a
+figure()
+plot(t(1:100), x(1:100))
+xlabel('Time (s)')
+ylabel('X values')
+title('X Vals. vs. Time (s)')
+grid on
