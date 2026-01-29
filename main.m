b807beb8db5d117a2c9965d15cd85b2294b46905
zwojtalik <zawo6371@colorado.edu>
2026-01-29 09:29:25 -0700

    asd

--- main.m
+++ main.m
@@ -5,6 +5,24 @@ clear
 %% Getting variables
 load("lab04_analysis_signal1.mat");
 
+%% Prelab
+fs = 2048;
+t = 0:1/fs:1-1/fs;
+x = cos(2*pi*t*300);
+
+N = length(x);
+xdft = fft(x);
+xdft = xdft(1:N/2+1);
+psdx = (1/(fs*N)) * abs(xdft).^2;
+psdx(2:end-1) = 2*psdx(2:end-1);
+freq = 0:fs/length(x):fs/2;
+
+plot(freq,pow2db(psdx))
+grid on
+title("Periodogram Using FFT")
+xlabel("Frequency (Hz)")
+ylabel("Power/Frequency (dB/Hz)")
+
 %% Problem 4.1 
 % a
 figure()
